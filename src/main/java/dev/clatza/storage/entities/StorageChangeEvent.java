d19ac5d84f2f853215f915018837c73105cd2c12
Chris <christopher@clatza.dev>
2021-01-01 23:35:01 +0100

    add static

--- src/main/java/dev/clatza/storage/entities/StorageChangeEvent.java
+++ src/main/java/dev/clatza/storage/entities/StorageChangeEvent.java
@@ -30,4 +30,8 @@ public class StorageChangeEvent extends Event {
     public HandlerList getHandlers() {
         return HANDLERS;
     }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
 }
